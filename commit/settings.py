d016dc9df7d88a1389bd024573c5228e85955cb7
cry0genic <cry0genic1103@gmail.com>
2021-05-20 12:19:46 +0530

    yaml file ughh

--- commit/settings.py
+++ commit/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") #change back to static once deployed ;-;
 
 #won't be used this time ;-;
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
